f01dcd456d42ebbafa93f8c5e361f8463596dcd8
ranamby7 <nambinintsoa.razafindrafito@gmail.com>
2017-02-19 18:15:45 +0300

    Update UtilDB.java

--- src/main/java/utilDB/UtilDB.java
+++ src/main/java/utilDB/UtilDB.java
@@ -8,9 +8,9 @@ public class UtilDB
 {
     public static Connection getPostgresConnection() throws ClassNotFoundException, SQLException
     {
-        Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://localhost:5432/freeziks";
-        Connection conn = DriverManager.getConnection(url, "postgres", "root");
+       Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://ec2-54-75-249-162.eu-west-1.compute.amazonaws.com:5432/d3ga2duri9utgj?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+        Connection conn = DriverManager.getConnection(url, "zdymfuwbsaeywh", "b3582156c187362dbfceff3dccffcb75a751506311aeb819186374276221bdba");
         return conn;
     }
 }
